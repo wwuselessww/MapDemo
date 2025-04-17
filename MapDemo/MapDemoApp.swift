fadf665ef235818c1059577c0ffbd5c236c1a7e2
wwuselessww <sa24122@gmail.com>
2025-04-17 15:13:55 +0300

    Initial Commit

---
+++ MapDemo/MapDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapDemoApp.swift
+//  MapDemo
+//
+//  Created by Alexander Kozharin on 17.04.25.
+//
+
+import SwiftUI
+
+@main
+struct MapDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
