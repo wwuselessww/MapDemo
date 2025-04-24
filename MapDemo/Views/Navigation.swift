6057b37aed49b80b2c2c41525aab588c37e28e67
wwuselessww <sa24122@gmail.com>
2025-04-24 19:15:41 +0300

    added navigation routing

--- MapDemo/Views/Navigation.swift
+++ MapDemo/Views/Navigation.swift
@@ -2,18 +2,16 @@ import SwiftUI
 import MapKit
 
 struct Navigation: View {
-    var source: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 40.740979140527834, longitude: -73.98966672823792)
-    var destination: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 40.73130329269146, longitude: -73.99705445144045)
-    var destination2: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 40.73130329269146, longitude: -73.98705445144045)
+    @EnvironmentObject var mainVm: MainViewModel
     @ObservedObject var vm = NavigationViewModel()
     var body: some View {
         VStack {
-            if let route = vm.route {
+            if let route = mainVm.route {
                 Map {
-                    Marker(item: MKMapItem(placemark: .init(coordinate: source)))
+                    Marker(mainVm.sourceTextfield, coordinate: mainVm.from)
                     MapPolyline(route)
                         .stroke(.blue, lineWidth: 5)
-                    Marker(item: MKMapItem(placemark: .init(coordinate: destination2)))
+                    Marker(mainVm.destinationTextfield, coordinate: mainVm.to)
                 }
             } else {
                 RoundedRectangle(cornerRadius: 2)
@@ -22,13 +20,9 @@ struct Navigation: View {
             
             
         }
-        .onAppear {
-            Task {
-                await vm.route = vm.setRoute(from: source, to: destination2)
-            }
-        }
     }
 }
 #Preview {
     Navigation()
+        .environmentObject(MainViewModel())
 }
